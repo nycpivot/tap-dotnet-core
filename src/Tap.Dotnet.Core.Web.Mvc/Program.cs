98e6da29a85bc89ca8c114d6b96bb5fe14b4d279
nycpivot <mijames@vmware.com>
2023-08-28 12:14:15 -0400

    Ongoing changes.

--- src/Tap.Dotnet.Core.Web.Mvc/Program.cs
+++ src/Tap.Dotnet.Core.Web.Mvc/Program.cs
@@ -8,9 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var weatherApi = Environment.GetEnvironmentVariable("WEATHER_API") ?? "https://tap-dotnet-core-api-weather.default.run-aks.tap.nycpivot.com";
-var wavefrontUrl = Environment.GetEnvironmentVariable("WAVEFRONT_URL") ?? "https://vmwareprod.wavefront.com";
-var wavefrontToken = Environment.GetEnvironmentVariable("WAVEFRONT_TOKEN") ?? "bb074869-ed55-4a94-8607-b384cf8a39c1";
+var weatherApi = Environment.GetEnvironmentVariable("WEATHER_API");
+var wavefrontUrl = Environment.GetEnvironmentVariable("WAVEFRONT_URL");
+var wavefrontToken = Environment.GetEnvironmentVariable("WAVEFRONT_TOKEN");
 
 var envVariable = new EnvironmentVariable() { Key = "WEATHER_API", Value = weatherApi };
 var wfClient = new WavefrontDirectIngestionClient.Builder(wavefrontUrl, wavefrontToken).Build();
