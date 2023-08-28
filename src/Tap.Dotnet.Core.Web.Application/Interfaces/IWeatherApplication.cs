6e5f9f3afaba70fecaa2ffd937e5bb76e864bcfc
nycpivot <mijames@vmware.com>
2023-08-28 14:58:26 -0400

    Fixed missing interface implementation.

--- src/Tap.Dotnet.Core.Web.Application/Interfaces/IWeatherApplication.cs
+++ src/Tap.Dotnet.Core.Web.Application/Interfaces/IWeatherApplication.cs
@@ -5,6 +5,5 @@ namespace Tap.Dotnet.Core.Web.Application.Interfaces
     public interface IWeatherApplication
     {
         IList<WeatherForecastViewModel> GetRandomForecastsByEnvironment();
-        IList<WeatherForecastViewModel> GetRandomForecastsByClaim();
     }
 }
