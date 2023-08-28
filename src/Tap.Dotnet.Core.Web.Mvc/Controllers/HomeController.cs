116375d6d229f532e92a61320e85b75d5ceb5d78
nycpivot <mijames@vmware.com>
2023-08-28 10:59:41 -0400

    Added Application project to MVC.

--- src/Tap.Dotnet.Core.Web.Mvc/Controllers/HomeController.cs
+++ src/Tap.Dotnet.Core.Web.Mvc/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Collections;
 using System.Diagnostics;
-using System.Net;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using Tap.Dotnet.Core.Web.Mvc.Models;
 
 namespace Tap.Dotnet.Core.Web.Mvc.Controllers
@@ -21,99 +15,8 @@ namespace Tap.Dotnet.Core.Web.Mvc.Controllers
 
         public IActionResult Index()
         {
-            try
-            {
-                var serviceBindings = Environment.GetEnvironmentVariable("SERVICE_BINDING_ROOT");
-
-                var secretPath = Path.Combine(serviceBindings, "weather-api", "host");
-                ViewBag.SecretPath = secretPath;
-
-                var weatherApi = System.IO.File.ReadAllText(secretPath);
-                ViewBag.WeatherApi = weatherApi;
-
-                weatherApi = weatherApi.Trim();
-
-                using (var handler = new HttpClientHandler())
-                {
-                    handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
-                    {
-                        return true;
-                    };
-
-                    using (var httpClient = new HttpClient(handler))
-                    {
-                        httpClient.BaseAddress = new Uri(weatherApi);
-
-                        //var response = await httpClient.GetAsync("weatherforecast");
-                        //response.EnsureSuccessStatusCode();
-
-                        var response = httpClient.GetAsync("weatherforecast").Result;
-                        if (response.StatusCode == HttpStatusCode.OK)
-                        {
-                            var content = response.Content.ReadAsStringAsync().Result;
-                            var forecasts = JsonConvert.DeserializeObject<List<WeatherForecast>>(content);
-
-                            if (forecasts != null && forecasts.Count == 5)
-                            {
-                                ViewBag.Forecast1 = forecasts[0];
-                                ViewBag.Forecast2 = forecasts[1];
-                                ViewBag.Forecast3 = forecasts[2];
-                                ViewBag.Forecast4 = forecasts[3];
-                                ViewBag.Forecast5 = forecasts[4];
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError("Index", ex.StackTrace ?? ex.Message);
-            }
-
             return View();
         }
-
-        public IActionResult Export()
-        {
-            try
-            {
-                var variables = new List<EnvironmentVariable>();
-
-                var environment = Environment.GetEnvironmentVariables();
-                foreach (DictionaryEntry variable in environment)
-                {
-                    var ev = new EnvironmentVariable()
-                    {
-                        Key = variable.Key.ToString() ?? String.Empty,
-                        Value = variable.Value?.ToString() ?? String.Empty
-                    };
-
-                    variables.Add(ev);
-                }
-
-                ViewBag.Variables = variables.OrderBy(e => e.Key).ToList();
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError("Export", ex.StackTrace ?? ex.Message);
-            }
-
-            return View();
-        }
-
-        //private static bool ServerCertificateCustomValidation(HttpRequestMessage requestMessage, X509Certificate2? certificate, X509Chain? chain, SslPolicyErrors sslErrors)
-        //{
-        //    // It is possible to inspect the certificate provided by the server.
-        //    Console.WriteLine($"Requested URI: {requestMessage.RequestUri}");
-        //    Console.WriteLine($"Effective date: {certificate?.GetEffectiveDateString()}");
-        //    Console.WriteLine($"Exp date: {certificate?.GetExpirationDateString()}");
-        //    Console.WriteLine($"Issuer: {certificate?.Issuer}");
-        //    Console.WriteLine($"Subject: {certificate?.Subject}");
-
-        //    // Based on the custom logic it is possible to decide whether the client considers certificate valid or not
-        //    Console.WriteLine($"Errors: {sslErrors}");
-        //    return sslErrors == SslPolicyErrors.None;
-        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
